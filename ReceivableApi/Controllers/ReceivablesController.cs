06a8150fdfd0844d54ee044fb8468148bffd7c67
Chris Caskey <cdcaskey@users.noreply.github.com>
2023-10-24 21:37:30 +0100

    Add Controller stubs

---
+++ ReceivableApi/Controllers/ReceivablesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReceivableApi.Data;
+using ReceivableApi.Models;
+using ReceivableApi.Models.Requests;
+using ReceivableApi.Models.Responses;
+
+namespace ReceivableApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceivablesController : ControllerBase
+    {
+        private const string DefaultCurrency = "USD";
+        private readonly ReceivableApiContext context;
+        private readonly Country[] countries;
+        private readonly Currency[] currencies;
+
+        public ReceivablesController(ReceivableApiContext context, CountryLoader countryLoader, CurrencyLoader currencyLoader)
+        {
+            this.context = context;
+            countries = countryLoader.Load();
+            currencies = currencyLoader.Load();
+        }
+
+        [HttpPost]
+        public IActionResult PostReceivables(List<AddReceivable> receivables)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("Summary/{currency?}")]
+        public ActionResult<ReceivableSummary> GetReceivableSummary(string? currency = DefaultCurrency)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ ReceivableApi/Models/Responses/ReceivableSummary.cs
@@ -0,0 +1,13 @@
+﻿namespace ReceivableApi.Models.Responses
+{
+    public class ReceivableSummary
+    {
+        public int OpenReceivables { get; set; }
+
+        public decimal TotalOpenReceivableValue { get; set; }
+
+        public int ClosedReceivables { get; set; }
+
+        public decimal TotalClosedReceivableValue { get; set; }
+    }
+}
